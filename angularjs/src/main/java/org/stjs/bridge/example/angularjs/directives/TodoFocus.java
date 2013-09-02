7cafebe7bef8f1cc5e2da225d691228ce0848bcd
Alexandru Craciun <ax.craciun@gmail.com>
2013-09-02 22:33:43 +0200

    modify to look closer to the initial version

--- angularjs/src/main/java/org/stjs/bridge/example/angularjs/directives/TodoFocus.java
+++ angularjs/src/main/java/org/stjs/bridge/example/angularjs/directives/TodoFocus.java
@@ -1,7 +1,5 @@
 package org.stjs.bridge.example.angularjs.directives;
 
-import static org.stjs.bridge.angularjs.GlobalAngularJS.$timeout;
-
 import org.stjs.bridge.angularjs.Attributes;
 import org.stjs.bridge.angularjs.Scope;
 import org.stjs.bridge.example.angularjs.App;
@@ -9,25 +7,26 @@ import org.stjs.javascript.annotation.Template;
 import org.stjs.javascript.functions.Callback2;
 import org.stjs.javascript.functions.Callback3;
 import org.stjs.javascript.functions.Function0;
+import org.stjs.javascript.functions.Function1;
 import org.stjs.javascript.jquery.JQueryCore;
 
 public class TodoFocus {
 
 	public static void main(String[] args) {
-		App.todomvc.directive("todoFocus", new Function0<Object>() {
+		App.todomvc.directive("todoFocus", new Function1<Callback3<Object, Object, Object>, Object>() {
 			@Override
-			public Object $invoke() {
+			public Object $invoke(final Callback3<Object, Object, Object> $timeout) {
 				return new Callback3<Scope, JQueryCore<JQueryCore<?>>, Attributes>() {
 					public void $invoke(final Scope scope, final JQueryCore<JQueryCore<?>> elem, final Attributes attrs) {
 						scope.$watch(attrs.$get("todoFocus"), new Callback2<Boolean, Boolean>() {
 							@Override
 							public void $invoke(Boolean newVal, Boolean oldVal) {
 								if (newVal) {
-									$timeout(new Function0<Boolean>() {
+									$timeout.$invoke(new Function0<Boolean>() {
 										@Override
 										@Template("invoke")
 										public Boolean $invoke() {
-											elem.focus();
+											elem.$get(0).focus();
 											return true;
 										}
 									}, 0, false);
